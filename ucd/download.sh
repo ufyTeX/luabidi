3542c9dc940d1b437cf77b46e5eab7624ca10de8
Deepak Jois <deepak.jois@gmail.com>
2016-09-18 11:37:15 +0530

    Fix typo

--- ucd/download.sh
+++ ucd/download.sh
@@ -2,4 +2,4 @@
 
 # Download BidiCharacterTest.txt and BidiTest.txt
 wget www.unicode.org/Public/9.0.0/ucd/BidiTest.txt
-whet www.unicode.org/Public/9.0.0/ucd/BidiCharacterTest.txt
+wget www.unicode.org/Public/9.0.0/ucd/BidiCharacterTest.txt
