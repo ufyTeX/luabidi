0cd9ccf341cf182170cb3d72263af0adfb1603ff
Deepak Jois <deepak.jois@gmail.com>
2016-09-18 12:05:24 +0530

    Top-level comment for bidi.lua

--- src/bidi.lua
+++ src/bidi.lua
@@ -1,7 +1,46 @@
 local ucdn = require("ucdn")
 local bracket = require("bidi.bracket")
 
--- FIXME Write some top-level docs.
+-- This implementation is a port based on the reference implementation found at:
+-- http://www.unicode.org/Public/PROGRAMS/BidiReferenceJava/
+--
+-- described in Unicode Bidirectional Algorithm (UAX #9).
+--
+-- Input:
+-- There are two levels of input to the algorithm, since clients may prefer to
+-- supply some information from out-of-band sources rather than relying on the
+-- default behavior.
+--
+-- - Bidi class array
+-- - Bidi class array, with externally supplied base line direction
+--
+-- Output:
+-- Output is separated into several stages:
+--
+--  - levels array over entire paragraph
+--  - reordering array over entire paragraph
+--  - levels array over line
+--  - reordering array over line
+--
+-- Note that for conformance to the Unicode Bidirectional Algorithm,
+-- implementations are only required to generate correct reordering and
+-- character directionality (odd or even levels) over a line. Generating
+-- identical level arrays over a line is not required. Bidi explicit format
+-- codes (LRE, RLE, LRO, RLO, PDF) and BN can be assigned arbitrary levels and
+-- positions as long as the rest of the input is properly reordered.
+--
+-- As the algorithm is defined to operate on a single paragraph at a time, this
+-- implementation is written to handle single paragraphs. Thus rule P1 is
+-- presumed by this implementation-- the data provided to the implementation is
+-- assumed to be a single paragraph, and either contains no 'B' codes, or a
+-- single 'B' code at the end of the input. 'B' is allowed as input to
+-- illustrate how the algorithm assigns it a level.
+--
+-- Also note that rules L3 and L4 depend on the rendering engine that uses the
+-- result of the bidi algorithm. This implementation assumes that the rendering
+-- engine expects combining marks in visual order (e.g. to the left of their
+-- base character in RTL runs) and that it adjusts the glyphs used to render
+-- mirrored characters that are in RTL runs so that they render appropriately.
 
 local Paragraph = {}
 Paragraph.__index = Paragraph
